8c69d3fbc72a375ae14cb1ca52eb6a8cbbdc7fcd
Emmanuel Hateka <emmanuelhateka.com>
2022-06-28 21:22:28 +0000

    image silder in progress

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@ const bars = document.querySelector("#bars");
 const menuLinks = document.querySelector(".mobile-div");
 const close = document.querySelector("#close");
 
+// Slider Dom Elements
+const leftArrow = document.querySelector("#left-arrow");
+const rightArrow = document.querySelector("#right-arrow");
+const heroImages = document.querySelector(".hero-img");
+const imagesContainer = document.querySelector(".hero-img-container");
+
 bars.addEventListener("click", () => {
   menuLinks.style.display = "grid";
   bars.style.display = "none";
@@ -9,3 +15,6 @@ bars.addEventListener("click", () => {
     menuLinks.style.display = "none";
   });
 });
+
+// slider
+leftArrow.addEventListener("click", () => {});
